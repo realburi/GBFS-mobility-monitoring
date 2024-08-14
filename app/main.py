69bce2d76f5caa7e180a84619bd489667a035f1a
Buri Batjargal <37531956+realburi@users.noreply.github.com>
2024-08-14 11:46:12 +0200

    fix dashboard

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import os 
 import requests
 
@@ -8,21 +8,36 @@ questDB_host = os.environ.get('QUESTDB_HOST', 'http://localhost:9000')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/api/data')
-def get_data():
-    sql_query = "select * from my_table; order by  timestamp"
+    sql_query = "select * from my_table; order by timestamp"
     response = requests.get(
         f'{questDB_host}/exec',
         params={'query': sql_query}
     )
-    result = []
+    result = [] 
     keys = ['current_range_meters', 'is_disabled_count', 'is_reserved_count', 'city', 'timestamp']
     for data in response.json().get('dataset'):
         result.append(dict(zip(keys, data)))
 
-    return jsonify(result)
+    breda = [x for x in result if x.get('city') == 'breda']
+    almere = [x for x in result if x.get('city') == 'almere']
+    delft = [x for x in result if x.get('city') == 'delft']
+
+    return render_template('index.html', breda=breda, almere=almere, delft=delft)
+
+
+# @app.route('/api/data')
+# def get_data():
+#     sql_query = "select * from my_table; order by  timestamp"
+#     response = requests.get(
+#         f'{questDB_host}/exec',
+#         params={'query': sql_query}
+#     )
+#     result = []
+#     keys = ['current_range_meters', 'is_disabled_count', 'is_reserved_count', 'city', 'timestamp']
+#     for data in response.json().get('dataset'):
+#         result.append(dict(zip(keys, data)))
+
+#     return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
